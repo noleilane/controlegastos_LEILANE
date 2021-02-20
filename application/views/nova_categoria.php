55ed80313b3bf9b45a6bb2ae9c99b71c60a7231c
Leilane <53538519+noleilane@users.noreply.github.com>
2021-02-20 06:39:46 -0300

    Criando nova_categoria.php

---
+++ application/views/nova_categoria.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<title>Nova categoria</title>
+</head>
+<body>
+	<h1>Nova categoria</h1>
+  <form action="">
+    <label for="data">Nome: <input type="text" name="nome"></label><br>
+    <label for="descricao">Descrição: <input type="text" name="descricao"></label><br>
+    <button type="button">Enviar</button>
+    <button type="reset">Limpar</button>
+  </form>
+</body>
+</html>
