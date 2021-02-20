9f7a99d38f5b5ee472490dbdb69723a69e33b157
Leilane <53538519+noleilane@users.noreply.github.com>
2021-02-20 06:37:17 -0300

    Criando listar_categorias.php

---
+++ application/views/listar_categorias.php
@@ -0,0 +1,20 @@
+  
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<title>Listar categorias</title>
+</head>
+<body>
+	<h1>Lista de categorias</h1>
+  <table>
+    <tr>
+      <td>Transporte</td>
+      <td>Alimentação</td>
+      <td>Remédios</td>
+      <td>Telefone/Internet</td>
+      <td>Outros gastos</td>
+    </tr> 
+  </table>
+</body>
+</html>
