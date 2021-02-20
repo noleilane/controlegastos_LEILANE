f7b2c9c5c4115b02cc389706a4e120c902ba2155
Leilane <53538519+noleilane@users.noreply.github.com>
2021-02-20 06:06:44 -0300

    Criando novo_gasto.php

---
+++ application/views/novo_gasto.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<title>Novo gasto</title>
+</head>
+<body>
+	<h1>Novo gasto</h1>
+  <form action="/action_page.php">
+    <label for="data">Data: <input type="date" name="data"></label><br>
+    <label for="descricao">Descrição: <input type="text" name="descricao"></label><br>
+    <label for="valor">Valor: <input type="number" name="valor"></label><br>
+    <button type="button">Enviar</button>
+    <button type="reset">Limpar</button>
+  </form>
+</body>
+</html>
